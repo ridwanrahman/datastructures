9d91604396c68fdc8c8549c88e9041a0ac972c81
ridwanrahman <ridwan_rahman@ymail.com>
2017-06-27 20:13:07 +0600

    wrote comments on doubly linked list

--- linkedlist/DoublyLinkedList/Node.py
+++ linkedlist/DoublyLinkedList/Node.py
@@ -7,21 +7,27 @@ class Node:
     #method for setting the data field of the node
     def setData(self, data):
         self.data = data
+    #method for getting the data of the node
     def getData(self):
         return self.data
+    #method for setting the next location of the node
     def setNext(self, next):
         self.next = next
+    #method for getting the next location of the node
     def getNext(self):
         return self.next
+    #method that returns True if next node exists
     def hasNext(self):
         return self.next != None
+    #method for setting the previous location of a node
     def setPrev(self, prev):
         self.prev = prev
+    #method for getting the previous node
     def getPrev(self):
         return self.prev
+    #method that returns True if the previous location exists
     def hasPrev(self):
         return self.prev != None
-
-
+    #method for returning the data of a node as a string 
     def __str__(self):
         return ("Node(Data = %s)")%(self.data)
